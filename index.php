80d209964add6e1057b65db38ac48b922fac2ccb
DataGurtchumelia <datagurtchmelia@gmail.com>
2023-09-20 07:34:54 +0400

    Folder Structur and Navbar

---
+++ index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./style/style.css?v=<?php echo(time())?>">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <title>DarkEye | Home</title>
+</head>
+<body>
+    <header>
+      <nav>
+        <img id="cover" src="./images/Project.jpg" alt="">
+        <ul>
+          <a href="">
+
+            <li>
+              <i class="fa-solid fa-database"></i>
+              SQL Injection
+            </li>
+          </a>
+
+          <a href="">
+
+            <li>
+              <i class="fa-brands fa-js"></i>
+              XSS
+            </li>
+          </a>
+
+          </li>
+          <a href="">
+
+            <li>
+              <i class="fa-solid fa-folder-closed"></i>
+              LFI
+            </li>
+          </a>
+          <a href="">
+
+            <li>
+            <i class="fa-solid fa-shield-halved"></i>
+            MSF Builder
+            </li>
+          </a>
+          <a href="">
+
+            <li>
+              <i class="fa-solid fa-eye"></i>
+              Osint
+            </li>
+          </a>
+          <a href="">
+
+            <li>
+              <i class="fa-solid fa-viruses"></i>
+              Web Shell
+            </li>
+          </a>
+          <a href="">
+
+            <li>
+              <i class="fa-solid fa-terminal"></i>
+              Terminal
+            </li>
+          </a>
+        </ul>
+      </nav>
+    </header>
+
+
+  <script src="./javascript/script.js?v=<?php echo(time())?>"></script>
+</body>
+</html>
